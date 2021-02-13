5fbb6df96d8e47537705013539ee741ca8115c6d
Salvatore Aresco <57394934+arscslvt@users.noreply.github.com>
2021-02-13 23:40:15 +0100

    New version! - Klipp 2.0
    - Post
    - Users
    - Login
    - Sign Up

--- login.js
+++ login.js
@@ -1,6 +1,11 @@
 const signUp = document.getElementById("signUp")
 const loginP = document.getElementById("loginPage");
 const signP = document.getElementById("signupPage");
+const resetPass = document.getElementById("forgetPass");
+
+var db = firebase.firestore();
+
+resetPass.addEventListener("click", passwordReset);
 
 signUp.addEventListener("click", function(){
     if (loginP.style.display == 'none'){
@@ -15,3 +20,86 @@ signUp.addEventListener("click", function(){
 
     }
 })
+
+document.getElementById("loginBt").addEventListener("click", function(){
+
+    var email = document.getElementById("logemail").value;
+    var password = document.getElementById("logpassword").value;
+
+    loading.style.display = "initial";
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        alert("signedIn")
+    })
+    .catch((error) => {
+        loading.style.display = "none";
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert("Errore: " + errorCode + "\n" + errorMessage);
+    });
+})
+
+document.getElementById("signBt").addEventListener("click", function(){
+
+    var name = document.getElementById("rname").value;
+    var surname = document.getElementById("rsurname").value;
+    var email = document.getElementById("remail").value;
+    var password = document.getElementById("rpassword").value;
+
+    loading.style.display = "initial";
+
+    let data = {
+        name: name,
+        surname: surname,
+        email: email,
+        userImage: "generic/noUserImage.png"
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        let setDoc = db.collection("users").doc(user.uid).set(data);
+        alert("Hi " + name + ", go to login to access Klipp.");
+        // ...
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+    });
+})
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in
+        uid = user.uid;
+        console.log("logged: " + uid);
+        loading.style.display = "none";
+        window.location.assign("index.html");
+    } else {
+      // User is signed out
+      
+    }
+});
+
+function passwordReset(){
+    var auth = firebase.auth();
+    var emailAddress = document.getElementById("logemail").value;
+
+    if(emailAddress!=""){
+        auth.sendPasswordResetEmail(emailAddress).then(function() {
+            alert("Check your mailbox! We sent you a password reset link.");
+        }).catch(function(error) {
+            alert("Mmh, this is an unregisterd email. Try to sign up!");
+        });
+    }else{
+        alert("Type your email first.");
+    }
+    
+}
--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var db = firebase.firestore();
 
-db.collection("articles").get().then((querySnapshot) => {
+var username = document.getElementById("username");
+
+db.collection("articles").orderBy("date", "desc").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         var data = doc.data();
-        getArticles(data);
+        var id = doc.id;
+        getArticles(data, id);
     });
 });
 
@@ -14,36 +17,76 @@ db.collection("articles").get().then((querySnapshot) => {
 //         getArticles(data);
 //     });
 
-function getArticles(data){
+function getArticles(data, id){
     var article = document.createElement("div");
     var title = document.createElement("span");
     var by = document.createElement("div");
     var profilePicture = document.createElement("img");
     var author = document.createElement("span");
     var text = document.createElement("span");
+    var control = document.createElement("div");
+    var like = document.createElement("div");
+    var likeCount = document.createElement("span");
+    var comment = document.createElement("div");
+    var commentCount = document.createElement("span");
+
 
     by.appendChild(profilePicture);
     by.appendChild(author);
+    control.appendChild(like);
+    like.appendChild(likeCount);
+    control.appendChild(comment);
+    comment.appendChild(commentCount);
     article.appendChild(by);
     article.appendChild(title);
     article.appendChild(text);
+    // article.appendChild(control);
 
     title.innerText = data.title;
     author.innerText = data.author;
     text.innerText = data.text;
+    likeCount.innerText = data.like;
+    commentCount.innerText = data.like;
 
     profilePicture.src = "assets/noUserImage.png";
     by.classList = "articleBy";
     author.classList = "articleAuthor";
     article.classList = "article";
     title.classList = "articleTitle";
+    control.classList = "articleControls";
+    like.classList = "controlsLike";
+
+    like.id = "l." + id;
+    like.setAttribute("onclick", "setLike(this.id)");
+
+    comment.classList = "controlsComment";
     
     text.classList = "articleText";
 
     document.getElementById("articles").appendChild(article);
 }
 
+function setLike(id){
+    var aid = id.slice(2);
 
+    var postRef = db.collection("articles").doc(aid);
+
+    // Set the "capital" field of the city 'DC'
+    return postRef.update({
+        like: firebase.firestore.FieldValue.increment(1)
+    })
+    .then(() => {
+        console.log("Document successfully updated!\n: " + db.collection("articles").get().data().like);
+    })
+    .catch((error) => {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+    });
+}
+
+function setComment(id){
+
+}
 // STATIC FUNCTIONS
 const toTop = document.getElementById("toTop");
 
@@ -64,4 +107,155 @@ toTop.addEventListener("click", function(){
         top: 0,
         behavior: 'smooth'
     }) 
+})
+
+// let deferredPrompt;
+// const installApp = document.querySelector('#installApp');
+// // installApp.style.display = 'none';
+
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     console.log("install");
+//     // Prevent Chrome 67 and earlier from automatically showing the prompt
+//     e.preventDefault();
+//     // Stash the event so it can be triggered later.
+//     deferredPrompt = e;
+//     // Update UI to notify the user they can add to home screen
+//     installApp.style.display = 'block';
+  
+//     installApp.addEventListener('click', (e) => {
+//       // hide our user interface that shows our A2HS button
+//       installApp.style.display = 'none';
+//       // Show the prompt
+//       deferredPrompt.prompt();
+//       // Wait for the user to respond to the prompt
+//       deferredPrompt.userChoice.then((choiceResult) => {
+//           if (choiceResult.outcome === 'accepted') {
+//             console.log('User accepted the A2HS prompt');
+//           } else {
+//             console.log('User dismissed the A2HS prompt');
+//           }
+//           deferredPrompt = null;
+//         });
+//     });
+//   });
+// LOGIN FUNCTION
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in
+        uid = user.uid;
+        console.log("user: " + uid);
+
+        var docRef = db.collection("users").doc(uid);
+
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                username.innerText = doc.data().name;
+                getProfileImage(uid);
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch((error) => {
+            console.log("Error getting user:", error);
+        });
+    } else {
+        window.location.assign("login.html");
+      
+    }
+});
+
+document.getElementById("logoutBt").addEventListener("click", function(){
+    firebase.auth().signOut().then(() => {
+        window.location.assign("login.html");
+      }).catch((error) => {
+        alert(error);
+      });
+})
+
+function getProfileImage(uid){
+    var storage = firebase.storage();
+    var storageRef = storage.ref();
+    var image;
+
+    var docRef = db.collection("users").doc(uid);
+
+    docRef.get().then((doc) => {
+        // console.log("image uid: " + doc.data().userImage);
+        if (doc.exists) {
+            var image = doc.data().userImage;
+            var spaceRef = storageRef.child(image);
+
+        spaceRef.getDownloadURL().then(function(url) {
+            document.getElementById("userImage").src = url;
+
+            }).catch(function(error) {
+                alert("cannot get image: " + error);
+            });
+
+        } else {
+            // doc.data() will be undefined in this case
+            // alert("Cannot find your user image, try to contact us via email.");
+            alert(doc.data());
+        }
+    }).catch((error) => {
+        console.log("Error getting image:", error);
+    });
+
+    return;
+}
+
+var newTitle = document.getElementById("title");
+var newText = document.getElementById("text");
+var newPublish = document.getElementById("publish");
+
+newTitle.addEventListener("click", function(){
+    newText.style.display = 'initial';
+    newPublish.style.display = 'initial';
+})
+
+newTitle.addEventListener("focusout", function(){
+    if(newTitle.value == ""){
+        newText.style.display = 'none';
+        newPublish.style.display = 'none';
+    }
+})
+
+newPublish.addEventListener("click", function(){
+    var user = firebase.auth().currentUser;
+
+    var docRef = db.collection("users").doc(user.uid);
+
+    var currentdate = new Date();
+    var datetime = currentdate.getDate() + "/"
+                    + (currentdate.getMonth()+1)  + "/" 
+                    + currentdate.getFullYear() + " "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            var author = doc.data().name + " " + doc.data().surname;
+            db.collection("articles").add({
+                author: author,
+                title: newTitle.value,
+                text: newText.value,
+                like: 0,
+                date: datetime
+            })
+            .then((docRef) => {
+                console.log("Document written with ID: ", docRef.id);
+                window.location.reload();
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+            });
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
 })
