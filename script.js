e7b4d333022921eb0ed72dd422cf5dd365c01e65
Salvatore Aresco <57394934+arscslvt@users.noreply.github.com>
2021-02-14 14:25:52 +0100

    web app a2h

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var db = firebase.firestore();
 
+navigator.serviceWorker.register('/service-worker.js');
+
 var username = document.getElementById("username");
 
 db.collection("articles").orderBy("date", "desc").get().then((querySnapshot) => {
@@ -125,35 +127,16 @@ toTop.addEventListener("click", function(){
     }) 
 })
 
-// let deferredPrompt;
-// const installApp = document.querySelector('#installApp');
-// // installApp.style.display = 'none';
+let installPromptEvent;
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//     console.log("install");
-//     // Prevent Chrome 67 and earlier from automatically showing the prompt
-//     e.preventDefault();
-//     // Stash the event so it can be triggered later.
-//     deferredPrompt = e;
-//     // Update UI to notify the user they can add to home screen
-//     installApp.style.display = 'block';
-  
-//     installApp.addEventListener('click', (e) => {
-//       // hide our user interface that shows our A2HS button
-//       installApp.style.display = 'none';
-//       // Show the prompt
-//       deferredPrompt.prompt();
-//       // Wait for the user to respond to the prompt
-//       deferredPrompt.userChoice.then((choiceResult) => {
-//           if (choiceResult.outcome === 'accepted') {
-//             console.log('User accepted the A2HS prompt');
-//           } else {
-//             console.log('User dismissed the A2HS prompt');
-//           }
-//           deferredPrompt = null;
-//         });
-//     });
-//   });
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent Chrome <= 67 from automatically showing the prompt
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  installPromptEvent = event;
+  // Update the install UI to notify the user app can be installed
+//   document.querySelector('#install-button').disabled = false;
+});
 // LOGIN FUNCTION
 
 firebase.auth().onAuthStateChanged((user) => {
